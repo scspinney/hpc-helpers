e9cc86dfafbfdb66cb21c511286079d483628a98
Sean Spinney <sean.c.spinney@gmail.com>
2021-01-21 15:09:17 -0500

    Update init_interactive_node.sh

--- init_interactive_node.sh
+++ init_interactive_node.sh
@@ -8,15 +8,16 @@ trap ctrl_c INT
 
 function ctrl_c()
 { echo "Cancelling Job."
-  cat cancel_job_cedar.sh | ssh CEDAR
+  cat cancel_job.sh | ssh CEDAR
   exit
 }
 
 
-cat create_run_script.sh | ssh CEDAR
+cat run_job.sh | ssh CEDAR
 
 # SSH Tunneling
 # keep this running as long as you want to connect
+# you will have to change the port probably, keep an eye on the terminal
 ssh -L 8888:cdr767.int.cedar.computecanada.ca:8888 $USERNAME@cedar.computecanada.ca
 
 
